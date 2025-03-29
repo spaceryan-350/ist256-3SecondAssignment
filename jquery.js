99522796c1a01416c1760b30291e6be834a7011c
ryanpatterson <savors3_alive@icloud.com>
2025-03-29 15:37:59 -0400

    Changing html code

--- jquery.js
+++ jquery.js
@@ -1,26 +1,42 @@
-$("#start").on("click", function(){
-    let date = new Date()
-    let second = date.getSeconds()
-    let millisecond = date.getMillisecond()
+$(document).ready(function(){
+let date = new Date()
+    let time = date.getSeconds() + date.getMilliseconds()
+    let time1 = 0
+    let totalTime;
     let result;
-    if ($("#millisecond").val()== randomValue){
-        alert("We have a match")
-        result = "We have a match"
-    }
-    if ($("#number").val()== randomValue){
-            alert("We have a match")
-            result = "We have a match"
-        }
-        if ($("#number").val()== randomValue){
-                alert("We have a match")
-                result = "We have a match"
-            }
 
-    else
-    { alert("Mismatch, correct value was " + randomValue)
-    result = "Mismatch, correct value was " + randomValue
-    }
-    $("#result").append("<p>" + result + "</p>");
+$("#start").click(function(){
+console.log(event);
+if (time1 === 0){
+let time1 = time
+$(this).text("Start")
+}
+else{
+let time2 = date.getTime()
+    $(this).text("Start")
+        if ($("#totalTime").val()=== 3.0){
+        $('body').css("background", "green");
+        let totalTime = Math.abs(time2 - time1)
+            result = totalTime
+        }
+        else if ($("#totalTime").val()<= 0.2){
+        $('body').css("background", "blue");
+        let totalTime = Math.abs(time2 - time1)
+               result = totalTime
+            }
+          else if ($("#totalTime").val()<= 0.5){
+          $('body').css("background", "yellow");
+          let totalTime = Math.abs(time2 - time1)
+                        result = totalTime
+                    }
+        else{
+        result = totalTime
+         $('body').css("background", "red");
+         }
+         $("#result").append("<p>" + result + "</p>");
+}
     });
 
+
 });
+
