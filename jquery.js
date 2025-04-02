44624a925120442f011fb7c181d189376be0ab4d
ryanpatterson <savors3_alive@icloud.com>
2025-04-02 18:40:02 -0400

    Changing html code

--- jquery.js
+++ jquery.js
@@ -3,6 +3,8 @@ let date = new Date()
     let time1;
     let time2;
     let totalTime;
+    let seconds;
+    let milliseconds;
     const time = 3;
     let result;
 
@@ -11,14 +13,22 @@ console.log("Click1");
 
 
 if($("#start").val() == "Start"){
-let time1 = date.getSeconds() + date.getMilliseconds()
+let seconds = date.getSeconds()
+let milliseconds = date.getMilliseconds()
+time1 = seconds + (milliseconds / 1000);
+console.log(time1);
 $("#start").val("Stop");
+$("#result").hide();
+$('body').css("background", "white");
 }
 
 
 else if($("#start").val() == "Stop"){
 $("#start").val("Start");
-let time2 = date.getSeconds() + date.getMilliseconds()
+let seconds = date.getSeconds()
+let milliseconds = date.getMilliseconds()
+let time2 = seconds + (milliseconds / 1000);
+console.log(time2);
 let totalTime = Math.abs(time2 - time1)
 console.log("Click2");
 
@@ -42,6 +52,7 @@ console.log("Click2");
         result = totalTime
          $('body').css("background", "red");
          $("#result").append("<p>" + result + "</p>");
+         $("#result").show();
          }
          }
 });
