5de0bb29fef5603363b6ffce86c5e29d001b7607
ryanpatterson <savors3_alive@icloud.com>
2025-04-03 14:01:08 -0400

    Changing html code

--- jquery.js
+++ jquery.js
@@ -1,59 +1,65 @@
 $(document).ready(function(){
-let date = new Date()
-    let time1;
-    let time2;
-    let totalTime;
-    let seconds;
-    let milliseconds;
-    const time = 3;
-    let result;
+let date;
+let time1;
+let time2;
+let totalTime;
+let seconds;
+let milliseconds;
+const time = 3.00;
+let result;
+let resultDisplay;
 
 $("#start").on("click", function(){
-console.log("Click1");
 
 
 if($("#start").val() == "Start"){
+let date = new Date()
 let seconds = date.getSeconds()
 let milliseconds = date.getMilliseconds()
-time1 = seconds + (milliseconds / 1000);
-console.log(time1);
+time1 = seconds + (milliseconds/1000)
 $("#start").val("Stop");
-$("#result").hide();
+$("#resultDisplay").hide();
 $('body').css("background", "white");
 }
 
 
 else if($("#start").val() == "Stop"){
-$("#start").val("Start");
+let date = new Date()
 let seconds = date.getSeconds()
 let milliseconds = date.getMilliseconds()
-let time2 = seconds + (milliseconds / 1000);
-console.log(time2);
-let totalTime = Math.abs(time2 - time1)
-console.log("Click2");
+let time2 = seconds + (milliseconds/1000)
+let totalTime = Math.abs(time2 - time1).toFixed(2)
+$("#start").val("Start");
 
-
-        if ($("#totalTime").val() === time){
+        if (totalTime === time){
         $('body').css("background", "green");
             result = totalTime
+            resultDisplay = totalTime
+            $("#resultDisplay").text("Time: " + result);
             $("#result").append("<p>" + result + "</p>");
        }
-        else if ($("#totalTime").val()<= 0.2){
+        else if (Math.abs(totalTime - time).toFixed(2) <= 0.20){
         $('body').css("background", "blue");
                result = totalTime
+               resultDisplay = totalTime
+                  $("#resultDisplay").text("Time: " + result);
                $("#result").append("<p>" + result + "</p>");
             }
-          else if ($("#totalTime").val()<= 0.5){
+          else if (Math.abs(totalTime - time).toFixed(2) <= 0.50){
           $('body').css("background", "yellow");
                         result = totalTime
+                        resultDisplay = totalTime
+                        $("#resultDisplay").text("Time: " + result);
                         $("#result").append("<p>" + result + "</p>");
                     }
         else{
         result = totalTime
+        resultDisplay = totalTime
          $('body').css("background", "red");
+         $("#resultDisplay").text("Time: " + result);
          $("#result").append("<p>" + result + "</p>");
-         $("#result").show();
          }
+         $("#resultDisplay").show();
          }
 });
 
